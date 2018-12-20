09cbabc3923bc277ae30342eb885da182765ce51
Jérémy Marodon <marodon.jeremy@gmail.com>
2018-12-20 17:13:31 +0100

    :sparkles: Can now inject logger without limitation

--- config/logger.php
+++ config/logger.php
@@ -3,5 +3,13 @@
 use Psr\Log\LoggerAwareInterface;
 use Symfony\Component\DependencyInjection\Reference;
 
-$container->registerForAutoconfiguration(LoggerAwareInterface::class)
-    ->addMethodCall('setLogger', array(new Reference('logger')));
+if ($container->hasDefinition('logger')) {
+    $class = new Reference('logger');
+} else {
+    $class = new \Psr\Log\NullLogger();
+}
+
+$container
+    ->registerForAutoconfiguration(LoggerAwareInterface::class)
+    ->addMethodCall('setLogger', [$class])
+;
