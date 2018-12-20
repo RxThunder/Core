a3e15b0e785da319d70146c1f08683e8598aa5cd
Jérémy Marodon <marodon.jeremy@gmail.com>
2018-12-20 18:16:14 +0100

    :fire: Remove dead code

--- config/container.php
+++ config/container.php
@@ -1,13 +1,8 @@
 <?php
 
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\ParameterBag\ContainerBag;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\DependencyInjection\Reference;
-
-$definition = new Definition();
-$definition->setPublic(false);
-$definition->setAutowired(true);
 
 $container
     ->autowire(ContainerBag::class, ContainerBag::class)
