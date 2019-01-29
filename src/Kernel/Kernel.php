172bcc18a12aa5c8885d789589ca7ff136223cda
Jérémy Marodon <marodon.jeremy@gmail.com>
2019-01-29 10:48:43 +0100

    :rocket: Release 0.4.8

--- src/Kernel/Kernel.php
+++ src/Kernel/Kernel.php
@@ -18,11 +18,11 @@ use Symfony\Component\Dotenv\Dotenv;
 class Kernel implements KernelInterface
 {
     const NAME = 'Thunder';
-    const VERSION = '0.4.7';
-    const VERSION_ID = 00407;
+    const VERSION = '0.4.8';
+    const VERSION_ID = '00408';
     const MAJOR_VERSION = 0;
     const MINOR_VERSION = 4;
-    const RELEASE_VERSION = 7;
+    const RELEASE_VERSION = 8;
     const EXTRA_VERSION = '';
 
     protected $environment;
