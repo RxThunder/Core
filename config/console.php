e80925f348019fbd7d36e71e7e2a3ac0dbedb324
Jérémy Marodon <marodon.jeremy@gmail.com>
2019-03-07 15:58:25 +0100

    :wrench: Remove an exclusion in console parameter setting

--- config/console.php
+++ config/console.php
@@ -11,4 +11,4 @@ $definition->setAutoconfigured(true);
 $container->registerForAutoconfiguration(AbstractConsole::class)
     ->addTag('console');
 
-$this->registerClasses($definition, 'RxThunder\\Core\\Console\\', '../src/Console', '../src/Console/{Application.php}');
+$this->registerClasses($definition, 'RxThunder\\Core\\Console\\', '../src/Console');
